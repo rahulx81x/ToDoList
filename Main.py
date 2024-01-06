b681c3c9fde1814ec6c1618bb34091429b5f4804
rahulx81x <rahulgouri072@gmail.com>
2024-01-06 11:37:30 +0530

    first actions

---
+++ Main.py
@@ -0,0 +1,6 @@
+import Utility as U
+pending = U.GetPendingTasks()
+print("!!! Welcome to your To-Do List !!!")
+print(" Here are your pending tasks --- ")
+U.DisplayTasks(pending)
+print(" What else would you like to do ??")
